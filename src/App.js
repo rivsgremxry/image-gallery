ba06debae588fda56706bbaef272ba8f95f1961d
Vitaliy <antylameer29@gmail.com>
2022-02-10 14:48:52 +0200

    Вывод компонента и названия

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Gallery from './Gallery';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1 className='AppName'> TEST APP </h1>
+      <Gallery />
     </div>
   );
 }
