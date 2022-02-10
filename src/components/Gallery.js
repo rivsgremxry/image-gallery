9517985188f3387780e5a7a6403d4a5a618e80bc
Vitaliy <antylameer29@gmail.com>
2022-02-10 16:42:40 +0200

    Открытие фотографии по клику

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '././Gallery.css'
 
 const Gallery = () => {
@@ -12,27 +12,37 @@ const Gallery = () => {
         { "id": 242, "url": "https://picsum.photos/id/242/300/200" }
     ]
 
+    const [modal, setModal] = useState(false)
+    const [tempUrl, setTempUrl] = useState('')
+
     const getImage = (url) => {
         console.warn(url);
+        setTempUrl(url)
+        setModal(true)
     }
 
     return (
-        <div className='gallery'>
-            {
-                data.map((item, index) => {
-                    return (
-                        <div className='picture' key={index}>
-                            <img
-                                src={item.url}
-                                alt={'image' + index}
-                                style={{ width: '100%' }}
-                                onClick={() => getImage(item.url)}
-                            />
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <>
+            <div className={modal ? "modal open" : "modal"}>
+                <img src={tempUrl} alt={tempUrl} />
+            </div>
+            <div className='gallery'>
+                {
+                    data.map((item, index) => {
+                        return (
+                            <div className='picture' key={index}>
+                                <img
+                                    src={item.url}
+                                    alt={'image' + index}
+                                    style={{ width: '100%' }}
+                                    onClick={() => getImage(item.url)}
+                                />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
 
