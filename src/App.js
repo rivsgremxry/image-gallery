a7dd60e9e2a49d74eef39c0f42aab1fc6008c32b
Vitaliy <antylameer29@gmail.com>
2022-02-11 16:12:12 +0200

    Removing unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Gallery from './components/Gallery';
-// import PostList from './components/PostList';
-// import PostForm from './components/PostForm';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  // const [posts, setPosts] = useState([
-  //   { id: 1, body: 'Body 1' },
-  //   { id: 2, body: 'Body 2' },
-  //   { id: 3, body: 'Body 3' },
-  // ])
-
-  // const createPost = (newPost) => {
-  //   setPosts([...posts, newPost])
-  // }
-
   return (
     <div className="App">
       <h1 className='AppName'> TEST APP </h1>
       <Gallery />
-      {/* <PostForm create={createPost}/> */}
-      {/* <PostList posts={posts} title="Post list 1" /> */}
     </div>
   );
 }
