7349b7de65215017c377dba5d541fe503d06c2d4
Vitaliy <antylameer29@gmail.com>
2022-02-11 18:18:29 +0200

    Added some comments

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,6 +6,7 @@ import { Modal } from 'react-bootstrap'
 
 const Gallery = () => {
 
+    // Массив с изображениями
     let data = [
         { "id": 237, "url": "https://picsum.photos/id/237/300/200" },
         { "id": 238, "url": "https://picsum.photos/id/238/300/200" },
@@ -15,32 +16,36 @@ const Gallery = () => {
         { "id": 242, "url": "https://picsum.photos/id/242/300/200" }
     ]
 
+    // Список комментариев
     const [posts, setPosts] = useState([
         { "id": 1, "name": "Name1", "body": "Test1" },
         { "id": 2, "name": "Name2", "body": "Test2" },
         { "id": 3, "name": "Name3", "body": "Test3" }
     ])
 
+    // Создание комментария
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
 
+    // Для того, чтобы открыть "копию" картинки в модальном окне
     const [tempUrl, setTempUrl] = useState('')
     const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
     const getImage = (url) => {
         setTempUrl(url)
         handleShow()
     }
+
+    // Модальное окно
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
             {/* <PostForm create={createPost} /> */}
             {/* <PostList posts={posts} /> */}
 
+            {/* Модальное окно с картинкой, списком комментариев и формой для создания комментариев */}
             <Modal show={show} onHide={handleClose} className='modal' centered size="lg">
                 <Modal.Body className='modal-body'>
                     <img src={tempUrl} alt={tempUrl} />
@@ -49,6 +54,7 @@ const Gallery = () => {
                 </Modal.Body>
             </Modal>
 
+            {/* Галерея изображений */}
             <div className='gallery'>
                 {
                     data.map((item, index) => {
