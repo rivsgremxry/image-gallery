aa0e28b85c75192ce1defcab6b0e0b120dce975e
Vitaliy <antylameer29@gmail.com>
2022-02-15 16:14:47 +0200

    Получение изображения и списка комментариев

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GetImages from './GetImages'
+import axios from 'axios'
 
 import '././Gallery.css'
 
 const Gallery = () => {
 
+    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        fetch("https://boiling-refuge-66454.herokuapp.com/images/237")
+            .then((res) => res.json())
+            .then(
+                (comments) => {
+                    setComments(comments);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
+    
+    console.log(JSON.stringify(comments))
+
     return (
         <>
-            <GetImages />
+            {/* <GetImages /> */}
+            <div>
+                {
+                    comments.map((item) => {
+                        return (
+                            <h1>{item.id}</h1>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
