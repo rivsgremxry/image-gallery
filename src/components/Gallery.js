14cb91bcf014bed8fa507cab5403ec797b8c23e3
Vitaliy <antylameer29@gmail.com>
2022-02-15 19:02:54 +0200

    Удален не нужный тестовый код

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,39 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import GetImages from './GetImages'
-import axios from 'axios'
 
 import '././Gallery.css'
 
 const Gallery = () => {
-
-    const [comments, setComments] = useState([]);
-    useEffect(() => {
-        fetch("https://boiling-refuge-66454.herokuapp.com/images/237")
-            .then((res) => res.json())
-            .then(
-                (comments) => {
-                    setComments(comments);
-                },
-                (error) => {
-                    console.log(error);
-                }
-            );
-    }, []);
-    
-    console.log(JSON.stringify(comments))
-
     return (
         <>
-            {/* <GetImages /> */}
-            <div>
-                {
-                    comments.map((item) => {
-                        return (
-                            <h1>{item.id}</h1>
-                        )
-                    })
-                }
-            </div>
+            <GetImages />
         </>
     )
 }
