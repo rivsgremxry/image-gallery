d600653a485a91ecb83c5416cbcc8dbec411b760
Vitaliy <antylameer29@gmail.com>
2022-02-17 13:03:16 +0200

    Добавлен вызов модального окна

--- src/components/GetImages.js
+++ src/components/GetImages.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 import { Modal } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 const GetItem = () => {
-  
+
   // Получение "обычных" изображений
   const [images, setImages] = useState([]);
   useEffect(() => {
@@ -19,42 +20,103 @@ const GetItem = () => {
       );
   }, []);
 
+  // Modal
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // Получение "больших" изображений и комментариев
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://boiling-refuge-66454.herokuapp.com/images/240")
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setData(data)
+        },
+        (error) => {
+          console.log(error)
+        }
+      );
+  }, []);
+
+  // Модальное окно
+  function handleModal() {
+    if (typeof data.comments !== 'undefined') {
+      for (let key of Object.keys(data.comments)) {
+        var d = new Date(data.comments[key].date);
+        let myDate = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear()
+        return (
+          <Modal show={show} onHide={handleClose} className='modal' centered size="lg">
+            <Modal.Body>
+              <img
+                src={data.url}
+                alt={'image' + data.id}
+                style={{ width: '100%' }}
+              />
+              <h1>{myDate}</h1>
+              <h1>{data.comments[key].text}</h1>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary">Close</Button>
+              <Button variant="primary">Save changes</Button>
+            </Modal.Footer>
+          </Modal>
+        )
+      }
+    }
+    if (!!data.comments) {
+      return (
+        <Modal.Dialog>
+          <Modal.Body>
+            <img
+              src={data.url}
+              alt={'image' + data.id}
+              style={{ width: '100%' }}
+            />
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            <Button variant="primary">Save changes</Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      )
+    }
+  }
+
+  // Получение ID и вызов модального окна
   const [show, setShow] = useState(false);
   function GetImageId(id) {
     console.log(id)
     handleShow()
   }
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
   return (
     <div className="App">
-      <>
-      <Modal show={show} onHide={handleClose} className='modal' centered size="lg">
-        <Modal.Body className='modal-body'>
-        </Modal.Body>
-      </Modal>
-      <div className="gallery">
-        {
-          images.map((item) => {
-            return (
-              <div className="picture" key={item.id}>
-                <img
-                  src={item.url}
-                  alt={'image' + item.id}
-                  style={{ width: '100%' }}
-                  onClick={() => GetImageId(item.id)}
-                />
-              </div>
-            )
-          })
-        }
-      </div>
-      </>
+
+        {handleModal()}
+
+        <div className="gallery">
+          {
+            images.map((item) => {
+              return (
+                <div className="picture" key={item.id}>
+                  <img
+                    src={item.url}
+                    alt={'image' + item.id}
+                    style={{ width: '100%' }}
+                    onClick={() => GetImageId(item.id)}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+
     </div>
   );
-  
+
 }
 
 export default GetItem
