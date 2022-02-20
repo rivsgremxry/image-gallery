3a447539f9a03d1150408dcc5fcc0cc7186c414d
Vitaliy <antylameer29@gmail.com>
2022-02-20 19:56:34 +0200

    Соединены: модальное окно, пост форма

--- src/components/GetImages.js
+++ src/components/GetImages.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react"
-
-import { Modal } from 'react-bootstrap'
-
-import { Container, Row, Col } from 'react-bootstrap'
+import Modal from "./Modal";
 
 const GetItem = () => {
 
@@ -22,99 +19,16 @@ const GetItem = () => {
   }, []);
 
   // Modal
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [modalOpen, setModalOpen] = useState(false)
+  const [currentPostId, setPostId] = useState(null)
 
-  // Получение "больших" изображений и комментариев
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://boiling-refuge-66454.herokuapp.com/images/238")
-      .then((res) => res.json())
-      .then(
-        (data) => {
-          setData(data)
-        },
-        (error) => {
-          console.log(error)
-        }
-      );
-  }, []);
-
-  // Модальное окно
-  function handleModal() {
-    if (typeof data.comments !== 'undefined') {
-      for (let key of Object.keys(data.comments)) {
-        var d = new Date(data.comments[key].date);
-        let myDate = d.getDate() + '.' + (d.getMonth() + 1) + '.' + d.getFullYear()
-        return (
-          <Modal show={show} onHide={handleClose} className='modal' centered size="lg">
-            <Modal.Body>
-              <Container>
-                <Row className="imageRow">
-                  <Col md={7}>
-                    <img
-                      src={data.url}
-                      alt={'image' + data.id}
-                      style={{ width: '100%' }}
-                    />
-                  </Col>
-                  <Col md={5} className="commentsCol">
-                    <p>{myDate}</p>
-                    <p>{data.comments[key].text}</p>
-                  </Col>
-                </Row>
-                <Row className="postRow">
-                  <Col md={{ span: 7, offset: 0 }}>
-                    <input className="inputs"></input>
-                    <input className="inputs"></input>
-                    <button className="inputs">Comment submit</button>
-                  </Col>
-                </Row>
-              </Container>
-            </Modal.Body>
-          </Modal>
-        )
-      }
-    }
-    if (!!data.comments) {
-      return (
-        <Modal show={show} onHide={handleClose} className='modal' centered size="lg">
-        <Modal.Body>
-          <Container>
-            <Row className="imageRow">
-              <Col md={12}>
-                <img
-                  src={data.url}
-                  alt={'image' + data.id}
-                  style={{ width: '100%' }}
-                />
-              </Col>
-            </Row>
-            <Row className="postRow">
-              <Col md={{ span: 12, offset: 0 }}>
-                <input className="inputs"></input>
-                <input className="inputs"></input>
-                <button className="inputs">Comment submit</button>
-              </Col>
-            </Row>
-          </Container>
-        </Modal.Body>
-      </Modal>
-      )
-    }
-  }
-
-  // Получение ID и вызов модального окна
-  const [show, setShow] = useState(false);
-  function GetImageId(id) {
-    console.log(id)
-    handleShow()
+  function handleModal(id) {
+    setModalOpen(true)
+    setPostId(id)
   }
 
   return (
     <div className="App">
-
-      {handleModal()}
 
       <div className="gallery">
         {
@@ -125,12 +39,13 @@ const GetItem = () => {
                   src={item.url}
                   alt={'image' + item.id}
                   style={{ width: '100%' }}
-                  onClick={() => GetImageId(item.id)}
+                  onClick={() => handleModal(item.id)}
                 />
               </div>
             )
           })
         }
+        { modalOpen &&  <Modal setModalOpen={ setModalOpen } postId={currentPostId} />}
       </div>
 
     </div>
